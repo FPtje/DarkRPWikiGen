ca041770b82ed094182b5a6e5bf354735f9094b5
Falco Peijnenburg <fpeijnenburg@gmail.com>
2020-08-26 20:07:42 +0200

    Keep index file to keep track of page names
    GMod lowercases all file names

--- darkrpwikiaddon/lua/drpwikigen.lua
+++ darkrpwikiaddon/lua/drpwikigen.lua
@@ -6,27 +6,26 @@ local functions = DarkRP.getStubs()
 -- IMPORTANT: These folders must be created manually on Linux
 -- Otherwise they will all be lowercase and the script will fail
 file.CreateDir("darkrpwiki")
-file.CreateDir("darkrpwiki/Functions")
-file.CreateDir("darkrpwiki/Functions/Darkrp/Server")
-file.CreateDir("darkrpwiki/Functions/Darkrp/Client")
-file.CreateDir("darkrpwiki/Functions/Darkrp/Shared")
-file.CreateDir("darkrpwiki/Functions/Player/Server")
-file.CreateDir("darkrpwiki/Functions/Player/Client")
-file.CreateDir("darkrpwiki/Functions/Player/Shared")
-file.CreateDir("darkrpwiki/Functions/Entity/Server")
-file.CreateDir("darkrpwiki/Functions/Entity/Client")
-file.CreateDir("darkrpwiki/Functions/Entity/Shared")
-file.CreateDir("darkrpwiki/Functions/Vector/Server")
-file.CreateDir("darkrpwiki/Functions/Vector/Client")
-file.CreateDir("darkrpwiki/Functions/Vector/Shared")
-file.CreateDir("darkrpwiki/Hooks/Server")
-file.CreateDir("darkrpwiki/Hooks/Client")
-file.CreateDir("darkrpwiki/Hooks/Shared")
+file.CreateDir("darkrpwiki/functions/darkrp/server")
+file.CreateDir("darkrpwiki/functions/darkrp/client")
+file.CreateDir("darkrpwiki/functions/darkrp/shared")
+file.CreateDir("darkrpwiki/functions/player/server")
+file.CreateDir("darkrpwiki/functions/player/client")
+file.CreateDir("darkrpwiki/functions/player/shared")
+file.CreateDir("darkrpwiki/functions/entity/server")
+file.CreateDir("darkrpwiki/functions/entity/client")
+file.CreateDir("darkrpwiki/functions/entity/shared")
+file.CreateDir("darkrpwiki/functions/vector/server")
+file.CreateDir("darkrpwiki/functions/vector/client")
+file.CreateDir("darkrpwiki/functions/vector/shared")
+file.CreateDir("darkrpwiki/hooks/server")
+file.CreateDir("darkrpwiki/hooks/client")
+file.CreateDir("darkrpwiki/hooks/shared")
 
 local realmSide = {
-	["Server"] = "Serverside",
-	["Client"] = "Clientside",
-	["Shared"] = "Shared"
+    ["Server"] = "Serverside",
+    ["Client"] = "Clientside",
+    ["Shared"] = "Shared"
 }
 
 local argumentsTemplate =
@@ -63,46 +62,55 @@ local singleParameterLayout =
 local inlineParamLayout = "{{Type|%s}} %s"
 
 local function getFileName(stub)
-	local kind = stub.metatable and "Functions" or "Hooks"
-	local realm = stub.realm
+    local kind = stub.metatable and "functions" or "hooks"
 
-	return string.format("darkrpwiki/%s/%s/%s.txt",
-		kind,
-		(stub.metatable and stub.metatable.MetaName .. "/" or "") .. stub.realm,
-		stub.name
-	)
+    return string.format("%s/%s/%s.txt",
+        kind,
+        string.lower((stub.metatable and stub.metatable.MetaName .. "/" or "") .. stub.realm),
+        string.lower(stub.name)
+    )
+end
+
+local function getWikiPageName(stub)
+    local kind = stub.metatable and "Functions" or "Hooks"
+
+    return string.format("%s/%s/%s",
+        kind,
+        (stub.metatable and stub.metatable.MetaName .. "/" or "") .. stub.realm,
+        stub.name
+    )
 end
 
 /*---------------------------------------------------------------------------
 Inline parameter list that just shows the types and names
 ---------------------------------------------------------------------------*/
 local function getInlineParams(tbl)
-	local params = {}
-	for k,v in pairs(tbl) do
-		table.insert(params, string.format(inlineParamLayout, v.type, v.name))
-	end
+    local params = {}
+    for k,v in pairs(tbl) do
+        table.insert(params, string.format(inlineParamLayout, v.type, v.name))
+    end
 
-	return table.concat(params, ', ')
+    return table.concat(params, ", ")
 end
 
 /*---------------------------------------------------------------------------
 Extended description of parameters
 ---------------------------------------------------------------------------*/
 local function getParameters(tbl)
-	if #tbl == 0 then return nil end
+    if #tbl == 0 then return nil end
 
-	local params = {}
-	for k,v in pairs(tbl) do
-		table.insert(params,
-			string.format(singleParameterLayout,
-				k, v.name,
-				k, v.type,
-				k, v.description
-			)
-		)
-	end
+    local params = {}
+    for k,v in pairs(tbl) do
+        table.insert(params,
+            string.format(singleParameterLayout,
+                k, v.name,
+                k, v.type,
+                k, v.description
+            )
+        )
+    end
 
-	return string.format(parameterListTemplate, table.concat(params))
+    return string.format(parameterListTemplate, table.concat(params))
 end
 
 -- hookname, realm-side, hookname, hookname, inlineArgs, description, realm-side, inlineReturns, parameters, return values
@@ -111,12 +119,12 @@ local hookWikiLayout =
 "[[Category:%s|%s]]\n" ..
 [[
 {{Lua_hook |
-	hook_name	= %s |
-	arguments	= %s |
-	description	= %s |
-	deprecated	= %s |
-	lua_state	= {{%s}} |
-	returns = %s |
+    hook_name   = %s |
+    arguments   = %s |
+    description = %s |
+    deprecated  = %s |
+    lua_state   = {{%s}} |
+    returns = %s |
 }}
 
 == Parameters ==
@@ -126,27 +134,27 @@ local hookWikiLayout =
 ]]
 
 local function getHookWikiPage(stub)
-	local parameters = getParameters(stub.parameters)
-	local returns = getParameters(stub.returns)
+    local parameters = getParameters(stub.parameters)
+    local returns = getParameters(stub.returns)
 
-	local inlineReturns = getInlineParams(stub.returns)
-	inlineReturns = inlineReturns ~= "" and inlineReturns or "{{Type|nil}}"
+    local inlineReturns = getInlineParams(stub.returns)
+    inlineReturns = inlineReturns ~= "" and inlineReturns or "{{Type|nil}}"
 
-	return string.format(hookWikiLayout,
-		stub.name,
-		realmSide[stub.realm],
-		stub.name,
-		stub.name,
-		getInlineParams(stub.parameters),
-		stub.description,
-		stub.deprecated or "",
-		realmSide[stub.realm],
-		inlineReturns,
-		parameters or "This hook isn't called with any parameters",
-		returns or
-			"This hook does not accept return values."..
-			" You can still return a value to override the default implementation of this hook."
-	)
+    return string.format(hookWikiLayout,
+        stub.name,
+        realmSide[stub.realm],
+        stub.name,
+        stub.name,
+        getInlineParams(stub.parameters),
+        stub.description,
+        stub.deprecated or "",
+        realmSide[stub.realm],
+        inlineReturns,
+        parameters or "This hook isn't called with any parameters",
+        returns or
+            "This hook does not accept return values." ..
+            " You can still return a value to override the default implementation of this hook."
+    )
 end
 
 -- name, metatable, name, realm-side, name, metatable, function name, inlineArgs, description, metatable,
@@ -157,13 +165,13 @@ local functionWikiLayout =
 "[[Category:%s|%s]]\n" ..
 "[[Category:%s|%s]]\n" ..
 [[{{Lua_function |
-	function_name	= %s%s%s |
-	arguments	= %s |
-	description	= %s |
-	deprecated	= %s |
-	returns 	= %s |
-	library		= {{Type|%s}} |
-	lua_state	= {{%s}} |
+    function_name   = %s%s%s |
+    arguments   = %s |
+    description = %s |
+    deprecated  = %s |
+    returns     = %s |
+    library     = {{Type|%s}} |
+    lua_state   = {{%s}} |
 }}
 
 == Function parameters ==
@@ -174,56 +182,67 @@ local functionWikiLayout =
 ]]
 
 local function getFunctionArguments(stub)
-	if #stub.parameters == 0 then return nil end
+    if #stub.parameters == 0 then return nil end
 
-	local params = {}
-	for k,v in pairs(stub.parameters) do
-		table.insert(params,
-			string.format(singleArgumentLayout,
-				k, v.name,
-				k, v.type,
-				k, not v.optional,
-				k, v.description
-			)
-		)
-	end
+    local params = {}
+    for k,v in pairs(stub.parameters) do
+        table.insert(params,
+            string.format(singleArgumentLayout,
+                k, v.name,
+                k, v.type,
+                k, not v.optional,
+                k, v.description
+            )
+        )
+    end
 
-	return string.format(argumentsTemplate, table.concat(params))
+    return string.format(argumentsTemplate, table.concat(params))
 end
 
 local function getFunctionWikiPage(stub)
-	local inlineReturns = getInlineParams(stub.returns)
-	inlineReturns = inlineReturns ~= "" and inlineReturns or "{{Type|nil}}"
+    local inlineReturns = getInlineParams(stub.returns)
+    inlineReturns = inlineReturns ~= "" and inlineReturns or "{{Type|nil}}"
 
-	return string.format(functionWikiLayout,
-		stub.name,
-		stub.metatable.MetaName,
-		stub.name,
-		realmSide[stub.realm],
-		stub.name,
-		stub.metatable.MetaName,
-		stub.metatable.MetaName == "DarkRP" and "." or ":",
-		stub.name,
-		getInlineParams(stub.parameters),
-		stub.description,
-		stub.deprecated or "",
-		inlineReturns,
-		stub.metatable.MetaName,
-		realmSide[stub.realm],
-		getFunctionArguments(stub) or "This function does not take any arguments.",
-		getParameters(stub.returns) or "This function does not return any value."
-	)
+    return string.format(functionWikiLayout,
+        stub.name,
+        stub.metatable.MetaName,
+        stub.name,
+        realmSide[stub.realm],
+        stub.name,
+        stub.metatable.MetaName,
+        stub.metatable.MetaName == "DarkRP" and "." or ":",
+        stub.name,
+        getInlineParams(stub.parameters),
+        stub.description,
+        stub.deprecated or "",
+        inlineReturns,
+        stub.metatable.MetaName,
+        realmSide[stub.realm],
+        getFunctionArguments(stub) or "This function does not take any arguments.",
+        getParameters(stub.returns) or "This function does not return any value."
+    )
 end
 
+local fileIndex = util.JSONToTable(file.Read("darkrpwiki/index.txt") or "{}")
+
 for k,v in pairs(hooks) do
-	if CLIENT and v.realm == "Shared" then continue end
-	print(getFileName(v))
-	file.Write(getFileName(v), getHookWikiPage(v))
+    if CLIENT and v.realm == "Shared" then continue end
+
+    local wikiName, fileName = getWikiPageName(v), getFileName(v)
+
+    fileIndex[wikiName] = fileName
+    print(wikiName, fileName)
+    file.Write("darkrpwiki/" .. fileName, getHookWikiPage(v))
 end
 
 for k,v in pairs(functions) do
-	if CLIENT and v.realm == "Shared" then continue end
-	print(getFileName(v))
+    if CLIENT and v.realm == "Shared" then continue end
 
-	file.Write(getFileName(v), getFunctionWikiPage(v))
+    local wikiName, fileName = getWikiPageName(v), getFileName(v)
+
+    fileIndex[wikiName] = fileName
+    print(wikiName, fileName)
+    file.Write("darkrpwiki/" .. fileName, getFunctionWikiPage(v))
 end
+
+file.Write("darkrpwiki/index.txt", util.TableToJSON(fileIndex, true))
